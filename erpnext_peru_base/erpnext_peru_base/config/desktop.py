ed806ab58e6d2ea7531458ae4d8af84ce4d97e37
Liberorbis Sistemas SAC <informes@liberorbis.com>
2015-02-25 12:24:12 -0500

    Apps
    Apps - Luis Paredes

---
+++ erpnext_peru_base/erpnext_peru_base/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+def get_data():
+	return {
+		"erpnext_peru_base": {
+			"color": "blue",
+			"icon": "octicon octicon-zap",
+			"type": "module",
+			"label": _("erpnext_peru_base")
+		}
+	}
